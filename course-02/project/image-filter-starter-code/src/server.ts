ee029559fbd04f79b93a4d7bfa3cb42d153878a6
Syeda Rezwana Hasan <syedahasan@syedas-mbp.home>
2021-02-14 23:09:53 -0600

    image filter updates

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,8 +1,10 @@
+import { Request, Response } from 'express';
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
+
 
   // Init the Express application
   const app = express();
@@ -16,21 +18,41 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
+  app.get('/filteredimage', async (req: Request, res: Response) => {
   // IT SHOULD
   //    1
   //    1. validate the image_url query
+      const { image_url } = req.query;
   //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
+      if (!image_url) {
+        return res.status(422).send("An image URL is required");
+      }
+ //    3. send the resulting file in the response
   //    4. deletes any files on the server on finish of the response
+      try {
+        const filteredImg = await filterImageFromURL(image_url);
+        //    3. send the resulting file in the response
+        res.status(200).sendFile(filteredImg, () => {
+          //    4. deletes any files on the server on finish of the response
+          deleteLocalFiles([filteredImg]);
+        });
+      } catch (error) {
+        res.status(406).send("Invalid URL");
+      }
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
+  // let { fileName } = req.params;
+  // const url = AWS.putImageUrl(fileName);
+  // res.status(201).send({url: url});
+});
   /**************************************************************************** */
 
   //! END @TODO1
   
+
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
